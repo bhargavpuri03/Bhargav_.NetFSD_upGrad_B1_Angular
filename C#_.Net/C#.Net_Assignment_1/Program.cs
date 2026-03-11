731e5720ae8113a1e55bb15c1b9751541be22f6e
Purimitlabhargav <bhargav.pu5974@gmail.com>
2026-03-11 17:43:11 +0530

    assignment 1

---
+++ C#_.Net/C#.Net_Assignment_1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace C_.Net_Assignment_1
+{
+    internal class first
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int div = a / b;
+            Console.WriteLine("quotient " + div);
+        }
+    }
+}
