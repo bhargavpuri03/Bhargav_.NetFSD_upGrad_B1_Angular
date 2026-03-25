61d7b1d3f821b1ee2e064a66d8c694f008d82c0e
Purimitlabhargav <bhargav.pu5974@gmail.com>
2026-03-25 12:17:25 +0530

    added

---
+++ C#_.Net/C#.Net_Multithreading_Assignment/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace C_.Net_LINQAssignment
+{
+    class BankAccount
+    {
+        public int Balance = 1000;
+        private object lockObj = new object();
+
+        public void WithdrawWithoutLock(int amount)
+        {
+            if (Balance >= amount)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " withdrawing " + amount);
+                Balance -= amount;
+                Console.WriteLine("Remaining Balance: " + Balance);
+            }
+        }
+
+        public void WithdrawWithLock(int amount)
+        {
+            lock (lockObj)
+            {
+                if (Balance >= amount)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " withdrawing " + amount);
+                    Balance -= amount;
+                    Console.WriteLine("Remaining Balance: " + Balance);
+                }
+                else
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name + " insufficient balance");
+                }
+            }
+        }
+    }
+
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+
+            Thread t1 = new Thread(() => account.WithdrawWithoutLock(700));
+            Thread t2 = new Thread(() => account.WithdrawWithoutLock(500));
+            Thread t3 = new Thread(() => account.WithdrawWithoutLock(400));
+
+            t1.Name = "User1";
+            t2.Name = "User2";
+            t3.Name = "User3";
+
+            Console.WriteLine("Without Lock:");
+            t1.Start(); t2.Start(); t3.Start();
+            t1.Join(); t2.Join(); t3.Join();
+
+            Console.WriteLine("\nWith Lock:");
+
+            account.Balance = 1000;
+
+            Thread t4 = new Thread(() => account.WithdrawWithLock(700));
+            Thread t5 = new Thread(() => account.WithdrawWithLock(500));
+            Thread t6 = new Thread(() => account.WithdrawWithLock(400));
+
+            t4.Name = "User1";
+            t5.Name = "User2";
+            t6.Name = "User3";
+
+            t4.Start(); t5.Start(); t6.Start();
+            t4.Join(); t5.Join(); t6.Join();
+        }
+    }
+}
---
+++ C#_.Net/C#.Net_Multithreading_Assignment/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace C_.Net_LINQAssignment
+{
+    internal class Program
+    {
+        static int[] partialSums = new int[5];
+
+        static void Process(object obj)
+        {
+            var data = (Tuple<List<int>, int>)obj;
+            var numbers = data.Item1;
+            int index = data.Item2;
+
+            int sum = 0;
+
+            Console.WriteLine("Thread " + (index + 1) + " processing:");
+            foreach (var n in numbers)
+            {
+                Console.Write(n + " ");
+                sum += n;
+            }
+
+            Console.WriteLine("\nThread " + (index + 1) + " Sum: " + sum);
+            partialSums[index] = sum;
+        }
+
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 50).ToList();
+
+            int size = 10;
+            Thread[] threads = new Thread[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                var part = numbers.Skip(i * size).Take(size).ToList();
+                threads[i] = new Thread(Process);
+                threads[i].Start(Tuple.Create(part, i));
+            }
+
+            for (int i = 0; i < 5; i++)
+                threads[i].Join();
+
+            int finalSum = partialSums.Sum();
+            Console.WriteLine("\nFinal Sum: " + finalSum);
+        }
+    }
+}
